98c1e770afa2702cd748e65e289ba103868a2304
codewithhamza <hamxa.javed311@gmail.com>
2023-06-19 18:19:14 +0500

    Change

--- little-lemon-table-reservation/src/Componentes/Main.js
+++ little-lemon-table-reservation/src/Componentes/Main.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Main.css";
 import heroimage from "../Assets/heroimg.jpg"
+import Menu from "./Menu";
+import GreekSalad from "../Assets/greek salad.jpg"
+import burshata from "../Assets/bursheta.png"
+import lemon from "../Assets/lemon dessert.jpg"
 
 function Main() {
   return (
@@ -27,6 +31,30 @@ function Main() {
             <div className="menu_title">
                 <h2>This week specials!</h2>
                 <a href="#">Online Menu</a>
+            </div>
+            <div className="menu_cards">
+                <Menu 
+                    imageSrc={GreekSalad}
+                    Title1= "Greek Salad"
+                    Title2= "$12.99"
+                    Para = "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
+                    Link= "Order a delivery"
+                />
+                   
+                <Menu 
+                    imageSrc={burshata}
+                    Title1= "Bruchetta"
+                    Title2= "$5.99"
+                    Para = "Our Bruschetta is very tasty and made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
+                    Link= "Order a delivery"
+                />
+                <Menu 
+                    imageSrc={lemon}
+                    Title1= "Lemon Dessert"
+                    Title2= "$5.00"
+                    Para = "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
+                    Link= "Order a delivery"
+                />
             </div>
         </section>
       </main>
